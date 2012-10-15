b007f5d40db053814765bb52a2dbb01e8836469a
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-10-15 17:26:56 -0300

    Added helper

--- lib/simple_menu.rb
+++ lib/simple_menu.rb
@@ -1,6 +1,11 @@
 require "simple_menu/helper"
 
 module SimpleMenu
+  def simple_menu(name = :default)
+    name = name.to_sym
+    @simple_menus ||= {}
+    @simple_menus[name] ||= SimpleMenu::Helper.new
+  end
 end
 
 ActionView::Base.send :include, SimpleMenu
