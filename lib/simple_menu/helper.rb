75cb132b28f2dde0618c6e0b731fec6c03c3a0bb
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-06-25 23:27:41 -0300

    Class to module

--- lib/simple_menu/helper.rb
+++ lib/simple_menu/helper.rb
@@ -1,71 +1,73 @@
-class SimpleMenu::Helper
-  
-  # can be initialized with the items passed through params
-  def initialize(params = {})
-    clear
-    params.each do |key, options|
-      add(key, options[0], options[1], options[2])
-    end
-  end
-  
-  def clear
-    @items = ActiveSupport::OrderedHash.new
-    self
-  end
-  
-  def add(key, label = nil, url = nil, params = {})
-    if key.kind_of? Hash
-      key.each do |key, options|
+module SimpleMenu
+  class Helper
+    
+    # can be initialized with the items passed through params
+    def initialize(params = {})
+      clear
+      params.each do |key, options|
         add(key, options[0], options[1], options[2])
       end
-    else
-      key = key.to_sym
-      if items.has_key? key
-        raise("key #{key} has already been taken")
-      end
-      
-      items[key] = [label, url, params]  
     end
     
-    self
-  end
-  
-  def items
-    @items
-  end
-  
-  def active=(key)
-    if key
-      @active = key.to_sym
-    else
-      @active = nil
+    def clear
+      @items = ActiveSupport::OrderedHash.new
+      self
     end
     
-    self
-  end
-  
-  def active
-    @active
-  end
-  
-  def to_s
-    html = []
-    
-    items.each do |key, options|
-      if key == active
-        html << '<li class="active">'
+    def add(key, label = nil, url = nil, params = {})
+      if key.kind_of? Hash
+        key.each do |key, options|
+          add(key, options[0], options[1], options[2])
+        end
       else
-        html << '<li>'
+        key = key.to_sym
+        if items.has_key? key
+          raise("key #{key} has already been taken")
+        end
+        
+        items[key] = [label, url, params]  
       end
       
-      html << "<a href=\"#{options[1]}\">#{options[0]}</a>"
-      html << '</li>'
+      self
     end
     
-    if html.length > 0
-      "<ul>#{html.join('')}</ul>"
-    else
-      ""
+    def items
+      @items
+    end
+    
+    def active=(key)
+      if key
+        @active = key.to_sym
+      else
+        @active = nil
+      end
+      
+      self
+    end
+    
+    def active
+      @active
+    end
+    
+    def to_s
+      html = []
+      
+      items.each do |key, options|
+        if key == active
+          html << '<li class="active">'
+        else
+          html << '<li>'
+        end
+        
+        html << "<a href=\"#{options[1]}\">#{options[0]}</a>"
+        html << '</li>'
+      end
+      
+      if html.length > 0
+        "<ul>#{html.join('')}</ul>"
+      else
+        ""
+      end
     end
   end
 end
